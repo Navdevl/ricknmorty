03e8dcf96be9a67bc047a5fcff9e0d644eb3c590
Naveen Honest Raj <naveen@skcript.com>
2019-02-01 02:54:19 +0530

    fixes in sorting
    Signed-off-by: Naveen Honest Raj <naveen@skcript.com>

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::MediaController < Api::V1::ApplicationController 
   skip_before_action :authorize
+  before_action :set_detail, except: [:movies]
 
   def index
-    @detail = params[:detail].present? # This parameter defines whether to show short coded or detailed view.
     if @detail
-      @media = Medium.all.includes(:submedia).latest
+      @media = Medium.all.includes(:submedia).latest.order('submedia.sub_id')
     else
       @media = Medium.all.latest
     end
@@ -15,11 +15,15 @@ class Api::V1::MediaController < Api::V1::ApplicationController
   end
 
   def seasons
-    @detail = params[:detail].present? # This parameter defines whether to show short coded or detailed view.
     if @detail
-      @media = Medium.season.includes(:submedia).latest
+      @media = Medium.season.includes(:submedia).latest.order('submedia.sub_id')
     else
       @media = Medium.season.latest
     end
+  end
+
+  private
+  def set_detail
+    @detail = params[:detail].present? # This parameter defines whether to show short coded or detailed view.
   end
 end
